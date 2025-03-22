866e48355b85fd5eb39ce4bb24d3996583db6401
Mahima Mohapatra <mahimamohapatra1@gmail.com>
2025-03-23 00:49:00 +0530

    fix: build fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts,scss}"
   ],
   theme: {
     extend: {
       colors: {
         primary: {
-          50: '#f0f9ff',
-          100: '#e0f2fe',
-          200: '#bae6fd',
-          300: '#7dd3fc',
-          400: '#38bdf8',
-          500: '#0ea5e9',
-          600: '#0284c7',
-          700: '#0369a1',
-          800: '#075985',
-          900: '#0c4a6e',
+          500: '#3B82F6',
+          600: '#2563EB',
+          700: '#1D4ED8',
         },
         secondary: {
-          50: '#fdf4ff',
-          100: '#fae8ff',
-          200: '#f5d0fe',
-          300: '#f0abfc',
-          400: '#e879f9',
-          500: '#d946ef',
-          600: '#c026d3',
-          700: '#a21caf',
-          800: '#86198f',
-          900: '#701a75',
-        },
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+        }
       },
       fontFamily: {
         sans: ['Inter var', 'sans-serif'],
